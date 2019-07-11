7d884a6df78b47bde30fe3aa821dc8068692c9f8
J0hn <j0hntvdesign@gmail.com>
2019-07-11 08:42:27 +0300

    Add table output

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import requests
 import logging
 import os
 from dotenv import load_dotenv
+from terminaltables import AsciiTable
 
 logging.basicConfig(level=logging.INFO, format='[%(levelname)s] - %(message)s')
 
-def get_area_id(city):
+def get_headhunter_area_id(city):
     url = 'https://api.hh.ru/suggests/areas'
     params = {'text': city}
     response = requests.get(url, params=params)
@@ -23,7 +24,7 @@ def predict_rub_salary(salary_from, salary_to):
 
 
 def get_hh_vacancies(language, area_id):   
-    logging.info(f'HeadHunter: search <{language}> vacancies...')
+    logging.info(f'Search <{language}> vacancies...')
     url = 'https://api.hh.ru/vacancies'
     page = 0
     salary = 0
@@ -50,13 +51,12 @@ def get_hh_vacancies(language, area_id):
         if page == response['pages']:
             break
 
-    return {'vacancies_found': found,
-            'average_salary': int(salary/found) if found else 'Нет вакансий'
-            }
+    return {'vacancies_found': found if found else 0,
+            'average_salary': int(salary/found) if found else 0}
 
 
 def get_superjob_vacancies(api_key, language, city):
-    logging.info(f'SuperJob: search <{language}> vacancies...')
+    logging.info(f'Search <{language}> vacancies...')
     url = 'https://api.superjob.ru/2.0/vacancies'
     page = 0
     salary = 0
@@ -89,34 +89,50 @@ def get_superjob_vacancies(api_key, language, city):
         if not response['more']:
             break
     
-    return {'vacancies_found': found,
-            'average_salary': int(salary/found) if found else 'Нет вакансий'
-            }
+    return {'vacancies_found': found if found else 0,
+            'average_salary': int(salary/found) if found else 0}
     
+
+def print_table(title, vacancies_dict, city):
+    columns = ['Язык программирования', 'Найдено вакансий', 'Средняя зарплата']
+    vacancies = [[language, 
+                vacancies_dict[language]['vacancies_found'],
+                vacancies_dict[language]['average_salary']] for language in vacancies_dict]
+
+    vacancies = sorted(vacancies, key=lambda x: x[2], reverse=True)
+
+    title = f'{title} {city}'
+
+    vacancies.insert(0, columns)
+    table_instance = AsciiTable(vacancies, title)
+    print(table_instance.table)
+
+
 
 if __name__ == '__main__':
     load_dotenv()
     SUPERJOB_KEY = os.getenv('SUPERJOB_KEY')
-    languages = ('Python', 'Java', 'Kotlin', 'C', 'C++', 'C#', 'Ruby', 'Go', '1С', 'JS', 'R', 'Swift', 'Scala', 'SQL')
+
+    languages = ('Python', 'Java', 'Kotlin',
+                'C', 'C++', 'C#', 'Ruby', 'Go',
+                '1С', 'JS', 'Php', 'R', 'Swift',
+                'Scala', 'SQL', 'Lua', 'Haskell',
+                'Bash', 'Pascal')
 
     city = 'Москва'
-    area_id = get_area_id(city)
+    city_headhunter_id = get_headhunter_area_id(city)
 
-    #=====HeadHunter======
-
+    print('[*] Собираем данные с HeadHunter')
     hh = {}
     for language in languages:
-        hh[language] = get_hh_vacancies(language, area_id)
+        hh[language] = get_hh_vacancies(language, city_headhunter_id)
     
-    for language, info in hh.items():
-        print(f'{language:8s} {info["average_salary"]}')
-
-    #=====SuperJob======
-    
-    
+    print('[*] Собираем данные с SuperJob')
     sj = {}
     for language in languages:
         sj[language] = get_superjob_vacancies(SUPERJOB_KEY, language, city)
-
-    for language, info in sj.items():
-        print(f'{language:8s} {info["average_salary"]}')
+    
+    print()
+    print_table('HeadHunter', hh, city)
+    print()
+    print_table('SuperJob', sj, city)
